209107c5adca3e344da289c9cb49c088afb6b4f1
Maya Parsons <parsons.may@northeastern.edu>
2026-09-15 15:04:56 -0400

    Add Spring Boot backend and health endpoint

---
+++ backend/src/test/java/com/mayaparsons/meowmatters/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayaparsons.meowmatters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
